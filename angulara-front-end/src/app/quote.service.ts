4342db78ce0cc4f9c80c8424d8724d0f6f54eaef
Derevyanko <aleksderev94@gmail.com>
2017-07-07 12:14:04 +0300

    Add CRUD operation

--- angulara-front-end/src/app/quote.service.ts
+++ angulara-front-end/src/app/quote.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Http, Response } from "@angular/http";
+import { Http, Response, Headers } from "@angular/http";
 import "rxjs/Rx";
 import { Observable } from "rxjs";
 
@@ -9,12 +9,33 @@ export class QuoteService {
 
 	}
 
+	API = "http://localhost:8080";
+
+	addQuote(content: string) {
+		const body = JSON.stringify({content: content});
+		const headers = new Headers({"Content-Type": "application/json"});
+		return this.http.post(`${this.API}/api/quote`, body, {headers: headers});
+	}
+
 	getQuotes(): Observable<any> {
-		return this.http.get('http://localhost:8080/api/quotes')
+		return this.http.get(`${this.API}/api/quotes`)
 			.map(
 				(response: Response) => {
 					return response.json().quotes;
 				}
 			);
+	}
+
+	updateQuote(id: number, newContent: string) {
+		const body = JSON.stringify({content: newContent});
+		const headers = new Headers({"Content-Type": "application/json"});
+		return this.http.put(`${this.API}/api/quote/${id}`, body, {headers: headers})
+			.map(
+				(response: Response) => response.json()
+			);
+	}
+
+	deleteQuote(id: number) {
+		return this.http.delete(`${this.API}/api/quote/${id}`);
 	}
 }
